f91db0c723e745b03035a6b03abff4e0b8132137
Filipe Santos Diniz <filipediniz465@gmail.com>
2025-02-05 17:27:41 -0300

    ex16

---
+++ HTML/Javascript/ex16/scripts/01arqjs.js
@@ -0,0 +1,6 @@
+let level = document.getElementById('Level')
+let masc = document.getElementById('masc')
+let fem = document.getElementById('fem')
+let texto = document.getElementById('texto')
+let imagem = document.getElementById('imagem')
+
