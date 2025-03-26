c6af2c1407bdd7c06b8bf865d707c82a4655fe7b
Filipe Santos Diniz <filipediniz465@gmail.com>
2025-03-26 16:53:28 -0300

    Atv-Pagina Cadastro

--- PHP/Filipe/Atividade 4/index.php
+++ PHP/Filipe/Atividade 4/index.php
@@ -1,11 +1,13 @@
 <html>
-
+ 
 <head>
   <meta charset="utf-8" />
   <title>App Help Desk</title>
-
+ 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+ 
+  <link rel="icon" href="logo.png" type="image/x-icon">
+ 
   <style>
     .card-login {
       padding: 30px 0 0 0;
@@ -14,33 +16,43 @@
     }
   </style>
 </head>
-
+ 
 <body>
-
+ 
   <nav class="navbar navbar-dark bg-dark">
     <a class="navbar-brand" href="#">
       <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
       App Help Desk
     </a>
   </nav>
-
+ 
   <div class="container">
     <div class="row">
-
+ 
       <div class="card-login">
         <div class="card">
           <div class="card-header">
             Login
           </div>
           <div class="card-body">
-            <form action="valida_login.php" method="get">
+            <form action="valida_login.php" method="GET">
               <div class="form-group">
-                <input type="email" class="form-control" placeholder="E-mail" name="email">
+                <img src="user.png" style="padding-bottom: 15px; width: 200px; margin-left:50px;" alt="Ícone de usuário">
+                <input name="email" type="email" class="form-control" placeholder="E-mail" autofocus>
               </div>
               <div class="form-group">
-                <input type="password" class="form-control" placeholder="Senha" name="senha">
+                <input name="senha" type="password" class="form-control" placeholder="Senha">
+                <div class="text-primary" style="text-align: right;">
+                  <a href="cadastro.php">Novo? Cadastre-se!</a>
+                </div>
               </div>
-              
+ 
+              <?php
+                //SE CADASTRO TIVER SIDO EFETUADO
+                if (isset($_GET['usuario']) && $_GET['usuario'] === 'sucesso') { ?>
+                  <script> alert('Usuário Cadastrado com sucesso!');</script>
+              <?php } ?>  
+             
               <?php
                 //VALIDA SE O PARÂMETRO LOGIN EXISTE E SE FOI AUTENTICADO
                 if (isset($_GET['login']) && $_GET['login'] === 'erro') { ?>
@@ -52,12 +64,7 @@
                 if (isset($_GET['login']) && $_GET['login'] === 'erro2') { ?>
                   <div class="text-danger"> Login obrigatório!</div>
               <?php } ?>
-
-          
-                       
-
-
-
+             
               <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
             </form>
           </div>
@@ -65,5 +72,6 @@
       </div>
     </div>
 </body>
-
+ 
 </html>
+ 
